f9ad0a7651a62e9f64bbc8f48256bd9d4a616773
Gipyo Hong <hgoeshard@gmail.com>
2025-05-06 12:34:10 +0900

    feat: mail table constants

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -5,3 +5,10 @@ export const MAIL_TEMPLATES = {
   },
 };
 export const MAIL_TEMPLATES_OPTIONS = Object.values(MAIL_TEMPLATES);
+
+export const EmailColumns = ['email'];
+export const EmailData = [
+  { email: 'dev@honggipyo.co.jp' },
+  { email: 'noreply@honggipyo.app' },
+  { email: 'hgoeshard@gmail.com' },
+];
